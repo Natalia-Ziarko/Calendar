b0f462aa8bee399d3405d47d96c2594482b67fa3
Natalia <nziarko1@gmail.com>
2024-11-20 14:15:28 +0100

    Basics

---
+++ app/main_view.php
@@ -0,0 +1,45 @@
+<?php require_once dirname(__FILE__) .'/../config.php';?>
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="pl" lang="pl">
+    
+<head>
+    <meta charset="utf-8" />
+    <title>Sports Events Calendar</title>
+    
+    <style>
+        .topnav {
+            padding: 10px;
+        }
+        
+        .header {
+            margin-top: 20px;
+            margin-bottom: 20px;
+        }
+        
+        .footer {
+            position: absolute;
+            bottom: 0;
+            padding: 10px;
+        }
+    </style>
+</head>
+    
+<body>
+    <div class="topnav">
+      <a>Home</a>
+      <a>Events</a>
+      <a>Players</a>
+      <a>Teams</a>
+      <a>Coaches</a>
+    </div>
+    
+    <div class="header">
+        Sports events calendar
+    </div>
+    
+    <div class="footer">
+        Created by Natalia Ziarko, 2024
+    </div>
+
+</body>
+</html>
