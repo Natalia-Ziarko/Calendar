287481026953bdcdab1aea4a16ccc1cb4c6b3db2
Natalia <nziarko1@gmail.com>
2024-11-28 09:56:52 +0100

    Added a feature to add new events through the form

--- new_event.php
+++ new_event.php
@@ -1,55 +1,63 @@
 <?php
 require_once dirname(__FILE__) . '/../config.php'; // Include config.php to access $db connection
 
-// Initialize the database connection
-Database::init($conf);  // Initialize with the configuration
-$db = Database::connectDB();  // Establish the database connection
-
-// Check if $db is null
-if ($db === null) {
-    die("Database connection failed");
-}
-
 const SYMBOL_COACH = "C";
 const SYMBOL_PLAYER = "P";
 const SYMBOL_TEAM = "T";
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $form_data = $_POST['form_data']; // Access the form data
+/**
+ * Handle submitted new event form
+ */
+function process_submitted_form() {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_GET['action']) && $_GET['action'] === 'new_event_form') {
+        // Extract form data
+        $f_event_name = filter_input(INPUT_POST, 'event_name', FILTER_SANITIZE_STRING);
+        $f_event_description = filter_input(INPUT_POST, 'event_description', FILTER_SANITIZE_STRING);
+        $f_event_type = filter_input(INPUT_POST, 'event_type', FILTER_SANITIZE_STRING);
+        $f_event_sport = filter_input(INPUT_POST, 'event_sport', FILTER_SANITIZE_STRING);
+        $f_event_venue = filter_input(INPUT_POST, 'event_venue', FILTER_SANITIZE_STRING);
+        $f_event_start_date = filter_input(INPUT_POST, 'event_start_date', FILTER_SANITIZE_STRING);
+        /*
+         * Unused field for the new event form
+         * $f_event_end_date = filter_input(INPUT_POST, 'event_end_date', FILTER_SANITIZE_STRING);
+         */
+        $f_event_team_1 = filter_input(INPUT_POST, 'event_team_1', FILTER_SANITIZE_STRING);
+        $f_event_team_2 = filter_input(INPUT_POST, 'event_team_2', FILTER_SANITIZE_STRING);
 
-    /* Form validation */
-    
-    // Not null attributes
-    $required_fields = [
-        'event_type', 
-        'event_sport', 
-        'event_name', 
-        'event_venue', 
-        'event_start_date', 
-        'event_team_1', 
-        'event_team_2'
-    ];
-    
-    $missing_fields = [];
-    
-    // Check for missing fields
-    foreach ($required_fields as $field) {
-        if (empty($form_data[$field])) {
-            $missing_fields[] = $field;
-        }
+        // Package the data into an array
+        $form_data = [
+            'event_name' => $f_event_name,
+            'event_description' => $f_event_description,
+            'event_type' => $f_event_type,
+            'event_sport' => $f_event_sport,
+            'event_venue' => $f_event_venue,
+            'event_start_date' => $f_event_start_date,
+            'event_end_date' => $f_event_end_date,
+            'event_team_1' => $f_event_team_1,
+            'event_team_2' => $f_event_team_2,
+        ];
+
+        // Call the PHP function and pass the form data
+        $result = process_new_event_request($form_data);
+
+        // Redirect to main.php with the result message
+        header("Location: /calendar/app/new_event_form_view.php?message=" . urlencode($result));
+
+        exit();
     }
-    
-    // Return an error with the missing fields and exit
-    if (!empty($missing_fields)) {
-        http_response_code(400); // Set HTTP response code to 400 (Bad Request)
-        
-        echo 'The following fields were not provided: ' . implode(', ', $missing_fields);
-        
-        exit;
-    }    
-    
-    /* Map form fields to the database column names */
-    
+}
+
+// Call the process_submitted_form function to process submitted new event form
+process_submitted_form();
+
+/**
+ * Process a new event request
+ *
+ * @param array $form_data
+ * @return string Returns an add event return message
+ */
+function process_new_event_request($form_data) {
+    // Map form fields to the sec_events column names
     $event_details = [
         'sec_ev_type_id' => $form_data['event_type'],
         'sec_ev_sport_id' => $form_data['event_sport'],
@@ -60,16 +68,27 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         'sec_ev_date_end' => $form_data['event_end_date']
     ];
     
+    // Map form fields to the sec_participants column names
     $event_particip = [
         'sec_pa_participant_id_1' => $form_data['event_team_1'],
         'sec_pa_participant_id_2' => $form_data['event_team_2']
     ];
     
-    /* Call the function to add the event to the database */
-  
-    add_event($event_details, $event_particip);
+    // Call the PHP function to add the event to the database
+    $result = add_event($event_details, $event_particip);
+
+    return $result;
 }
 
+/**
+ * Add event team to the database
+ *
+ * @global PDO $db The PDO database connection object
+ * @param int $event_id
+ * @param int $event_team
+ * @return string If an error occurs, returns a predefined error message (MSG_IF_ERROR)
+ * @throws Exception If there is a failure in fetching data
+ */
 function add_team_to_event($event_id, $event_team) {
     global $db;
     
@@ -92,17 +111,31 @@ function add_team_to_event($event_id, $event_team) {
         ]);
     
     } catch (Exception $e) {
-        $db->rollback(); // Rollback the transaction if an error occurs
+        // Rollback the transaction if an error occurs
+        $db->rollback();
         
-        echo 'Failed to add event participant ' . $e->getMessage();
+        error_log('Failed to add event participant: ' . $e->getMessage());
+        
+        return MSG_IF_ERROR;
     }
 }
 
+/**
+ * Add event to the database
+ *
+ * @global PDO $db The PDO database connection object
+ * @param array $event_details
+ * @param array $event_particip
+ * @return string Predefined success message (MSG_IF_DONE) or an error message (MSG_IF_ERROR)
+ *              If an error occurs, returns a predefined error message (MSG_IF_ERROR)
+ * @throws Exception If there is a failure in fetching data
+ */
 function add_event($event_details, $event_particip) {
     global $db;
     
     try {
-        $db->beginTransaction(); // Begin the transaction
+        // Begin the transaction
+        $db->beginTransaction();
         
         $sql = "INSERT INTO sec_events (
             sec_ev_type_id, 
@@ -124,30 +157,31 @@ function add_event($event_details, $event_particip) {
 
         $stmt = $db->prepare($sql);
         $stmt->execute($event_details);
-
-        $event_id = $db->lastInsertId(); // Retrieve the ID of the last inserted row
+        
+        // Retrieve the ID of the last inserted row
+        $event_id = $db->lastInsertId(); 
 
         if (!$event_id) {
-            $db->rollback(); // Rollback the transaction if no event ID is returned
+            // Rollback the transaction if no event ID is returned
+            $db->rollback();
             
-            echo 'Failed to add a new event';
-            
-            return; // Exit after rollback
+            return MSG_IF_ERROR;   
             
         } else {
-            /* Add teams to the event */
-            
+            ///Add teams to the event
             add_team_to_event($event_id, $event_particip['sec_pa_participant_id_1']);
             add_team_to_event($event_id, $event_particip['sec_pa_participant_id_2']);
             
             $db->commit();
             
-            echo 'Event added successfully';
+            return MSG_IF_DONE;
         }
     
     } catch (Exception $e) {
         $db->rollback();
         
-        echo 'Failed to add a new event ' . $e->getMessage();        
+        error_log('Failed to add a new event: ' . $e->getMessage());
+        
+        return MSG_IF_ERROR;        
     }
 }
